53187b01b95c108f4b72722d27dc5f2185759a0b
Leon <phaibin@gmail.com>
2014-02-26 15:34:14 +0800

    。。。

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,28 +1,36 @@
 class CitiesController < ApplicationController
   def index
-    result = {}
-    cities = City.all
-    hot_cities = []
-    cities.each do |city|
-      if city.is_hot
-        hot_cities << city
-      else
-        if city.name_en && city.name_en.strip.length > 0
-          index = city.name_en[0].upcase
-          result[index] ||= []
-          result[index] << city
+    @response = Response.new
+    result = nil
+    begin
+      result ||= {}
+      cities = City.all
+      hot_cities = []
+      cities.each do |city|
+        if city.is_hot
+          hot_cities << city
+        else
+          if city.name_en && city.name_en.strip.length > 0
+            index = city.name_en[0].upcase
+            result[index] ||= []
+            result[index] << city
+          end
         end
       end
-    end
-    result = Hash[result.sort.unshift(['hot', hot_cities])]
-    result.each { |k, v| v.sort_by!(&:name_en) }
+      result = Hash[result.sort.unshift(['hot', hot_cities])]
+      result.each { |k, v| v.sort_by!(&:name_en) }
 
-    @response = Response.new
-    @response.result = result
+      @response.code = 200
+      @response.message = 'success'
+    rescue
+      @response.code = 501
+      @response.message = 'error'
+    ensure
+      @response.result = result
+    end
   end
 
   def show
     @city = City.find(params[:id])
-    # render json: @city
   end
 end
--- app/views/cities/show.json.jbuilder
+++ app/views/cities/show.json.jbuilder
@@ -1,2 +1,4 @@
 json.id @city.id
 json.name_cn @city.name_cn
+json.latitude @city.latitude
+json.longitude @city.longitude
